48d3fae739a10d12e950c7c726066ff2b668111e
neerajaraju <60924403+neerajaraju@users.noreply.github.com>
2020-03-12 14:31:49 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,19 @@
+package springproject;
+
+
+public class Student
+{
+private String name;
+public String GetName()
+{
+return name;
+}
+public void setName(String name)
+{
+this.name=name;
+}
+public void displayinfo()
+{
+System.out.println("hello:"+name);
+}
+}
